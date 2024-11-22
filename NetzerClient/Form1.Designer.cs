f370d3e590ea8c11a027c80ed9447d584024619b
Eder Oliveira <ederoliveira152@gmail.com>
2024-11-22 11:46:40 -0300

    More info added

--- NetzerClient/Form1.Designer.cs
+++ NetzerClient/Form1.Designer.cs
@@ -34,12 +34,23 @@
             lblComputerNameField = new Label();
             lblUserNameField = new Label();
             groupBox1 = new GroupBox();
-            label3 = new Label();
             label2 = new Label();
             label1 = new Label();
-            label4 = new Label();
-            label5 = new Label();
+            lblRamField = new Label();
+            lblStorageField = new Label();
+            lblProcessorField = new Label();
+            lblOperatingSystemField = new Label();
+            lblOperatingSystem = new Label();
+            lblStorage = new Label();
+            lblRam = new Label();
+            lblProcessor = new Label();
+            groupBox2 = new GroupBox();
+            lblMacAddressField = new Label();
+            lblIpAddressField = new Label();
+            lblIpAddress = new Label();
+            lblMacAddress = new Label();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // lblComputerName
@@ -86,9 +97,9 @@
             lblComputerNameField.ForeColor = Color.FromArgb(224, 224, 224);
             lblComputerNameField.Location = new Point(208, 44);
             lblComputerNameField.Name = "lblComputerNameField";
-            lblComputerNameField.Size = new Size(171, 21);
+            lblComputerNameField.Size = new Size(174, 21);
             lblComputerNameField.TabIndex = 3;
-            lblComputerNameField.Text = "nome do computador";
+            lblComputerNameField.Text = "Nome do computador";
             lblComputerNameField.Click += label1_Click;
             // 
             // lblUserNameField
@@ -98,87 +109,208 @@
             lblUserNameField.ForeColor = Color.FromArgb(224, 224, 224);
             lblUserNameField.Location = new Point(169, 75);
             lblUserNameField.Name = "lblUserNameField";
-            lblUserNameField.Size = new Size(133, 21);
+            lblUserNameField.Size = new Size(136, 21);
             lblUserNameField.TabIndex = 4;
-            lblUserNameField.Text = "nome do usuário";
+            lblUserNameField.Text = "Nome do usuário";
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(lblRamField);
+            groupBox1.Controls.Add(lblStorageField);
+            groupBox1.Controls.Add(lblProcessorField);
+            groupBox1.Controls.Add(lblOperatingSystemField);
+            groupBox1.Controls.Add(lblOperatingSystem);
+            groupBox1.Controls.Add(lblStorage);
+            groupBox1.Controls.Add(lblRam);
+            groupBox1.Controls.Add(lblProcessor);
             groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             groupBox1.ForeColor = Color.FromArgb(224, 224, 224);
             groupBox1.Location = new Point(12, 110);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(508, 160);
+            groupBox1.Size = new Size(508, 132);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "Hardware information";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.FromArgb(224, 224, 224);
-            label3.ForeColor = Color.FromArgb(64, 64, 64);
-            label3.Location = new Point(16, 76);
-            label3.Name = "label3";
-            label3.Size = new Size(66, 21);
-            label3.TabIndex = 2;
-            label3.Text = "Storage:";
-            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.BackColor = Color.FromArgb(224, 224, 224);
-            label2.ForeColor = Color.FromArgb(64, 64, 64);
-            label2.Location = new Point(16, 97);
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(224, 224, 224);
+            label2.Location = new Point(325, 76);
             label2.Name = "label2";
-            label2.Size = new Size(47, 21);
-            label2.TabIndex = 1;
-            label2.Text = "RAM:";
+            label2.Size = new Size(96, 21);
+            label2.TabIndex = 10;
+            label2.Text = "Espaço livre";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.FromArgb(224, 224, 224);
             label1.ForeColor = Color.FromArgb(64, 64, 64);
-            label1.Location = new Point(16, 55);
+            label1.Location = new Point(231, 76);
             label1.Name = "label1";
-            label1.Size = new Size(81, 21);
-            label1.TabIndex = 0;
-            label1.Text = "Processor:";
+            label1.Size = new Size(88, 21);
+            label1.TabIndex = 9;
+            label1.Text = "Free Space:";
             // 
-            // label4
+            // lblRamField
             // 
-            label4.AutoSize = true;
-            label4.BackColor = Color.FromArgb(224, 224, 224);
-            label4.ForeColor = Color.FromArgb(64, 64, 64);
-            label4.Location = new Point(16, 34);
-            label4.Name = "label4";
-            label4.Size = new Size(150, 21);
-            label4.TabIndex = 3;
-            label4.Text = "Operational System:";
+            lblRamField.AutoSize = true;
+            lblRamField.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblRamField.ForeColor = Color.FromArgb(224, 224, 224);
+            lblRamField.Location = new Point(69, 97);
+            lblRamField.Name = "lblRamField";
+            lblRamField.Size = new Size(143, 21);
+            lblRamField.TabIndex = 8;
+            lblRamField.Text = "Memória utilizável";
             // 
-            // label5
+            // lblStorageField
             // 
-            label5.AutoSize = true;
-            label5.BackColor = Color.FromArgb(224, 224, 224);
-            label5.ForeColor = Color.FromArgb(64, 64, 64);
-            label5.Location = new Point(16, 118);
-            label5.Name = "label5";
-            label5.Size = new Size(26, 21);
-            label5.TabIndex = 4;
-            label5.Text = "IP:";
+            lblStorageField.AutoSize = true;
+            lblStorageField.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblStorageField.ForeColor = Color.FromArgb(224, 224, 224);
+            lblStorageField.Location = new Point(88, 76);
+            lblStorageField.Name = "lblStorageField";
+            lblStorageField.Size = new Size(142, 21);
+            lblStorageField.TabIndex = 7;
+            lblStorageField.Text = "Tamanho do disco";
+            // 
+            // lblProcessorField
+            // 
+            lblProcessorField.AutoSize = true;
+            lblProcessorField.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblProcessorField.ForeColor = Color.FromArgb(224, 224, 224);
+            lblProcessorField.Location = new Point(103, 55);
+            lblProcessorField.Name = "lblProcessorField";
+            lblProcessorField.Size = new Size(187, 21);
+            lblProcessorField.TabIndex = 6;
+            lblProcessorField.Text = "Modelo do processador";
+            // 
+            // lblOperatingSystemField
+            // 
+            lblOperatingSystemField.AutoSize = true;
+            lblOperatingSystemField.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblOperatingSystemField.ForeColor = Color.FromArgb(224, 224, 224);
+            lblOperatingSystemField.Location = new Point(160, 34);
+            lblOperatingSystemField.Name = "lblOperatingSystemField";
+            lblOperatingSystemField.Size = new Size(159, 21);
+            lblOperatingSystemField.TabIndex = 6;
+            lblOperatingSystemField.Text = "Sistema Operacional";
+            // 
+            // lblOperatingSystem
+            // 
+            lblOperatingSystem.AutoSize = true;
+            lblOperatingSystem.BackColor = Color.FromArgb(224, 224, 224);
+            lblOperatingSystem.ForeColor = Color.FromArgb(64, 64, 64);
+            lblOperatingSystem.Location = new Point(16, 34);
+            lblOperatingSystem.Name = "lblOperatingSystem";
+            lblOperatingSystem.Size = new Size(138, 21);
+            lblOperatingSystem.TabIndex = 3;
+            lblOperatingSystem.Text = "Operating System:";
+            // 
+            // lblStorage
+            // 
+            lblStorage.AutoSize = true;
+            lblStorage.BackColor = Color.FromArgb(224, 224, 224);
+            lblStorage.ForeColor = Color.FromArgb(64, 64, 64);
+            lblStorage.Location = new Point(16, 76);
+            lblStorage.Name = "lblStorage";
+            lblStorage.Size = new Size(66, 21);
+            lblStorage.TabIndex = 2;
+            lblStorage.Text = "Storage:";
+            // 
+            // lblRam
+            // 
+            lblRam.AutoSize = true;
+            lblRam.BackColor = Color.FromArgb(224, 224, 224);
+            lblRam.ForeColor = Color.FromArgb(64, 64, 64);
+            lblRam.Location = new Point(16, 97);
+            lblRam.Name = "lblRam";
+            lblRam.Size = new Size(47, 21);
+            lblRam.TabIndex = 1;
+            lblRam.Text = "RAM:";
+            // 
+            // lblProcessor
+            // 
+            lblProcessor.AutoSize = true;
+            lblProcessor.BackColor = Color.FromArgb(224, 224, 224);
+            lblProcessor.ForeColor = Color.FromArgb(64, 64, 64);
+            lblProcessor.Location = new Point(16, 55);
+            lblProcessor.Name = "lblProcessor";
+            lblProcessor.Size = new Size(81, 21);
+            lblProcessor.TabIndex = 0;
+            lblProcessor.Text = "Processor:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lblMacAddressField);
+            groupBox2.Controls.Add(lblIpAddressField);
+            groupBox2.Controls.Add(lblIpAddress);
+            groupBox2.Controls.Add(lblMacAddress);
+            groupBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox2.ForeColor = Color.FromArgb(224, 224, 224);
+            groupBox2.Location = new Point(12, 248);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(508, 88);
+            groupBox2.TabIndex = 10;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Network information";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // lblMacAddressField
+            // 
+            lblMacAddressField.AutoSize = true;
+            lblMacAddressField.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblMacAddressField.ForeColor = Color.FromArgb(224, 224, 224);
+            lblMacAddressField.Location = new Point(124, 55);
+            lblMacAddressField.Name = "lblMacAddressField";
+            lblMacAddressField.Size = new Size(203, 21);
+            lblMacAddressField.TabIndex = 6;
+            lblMacAddressField.Text = "Mac Adress do dispositivo";
+            // 
+            // lblIpAddressField
+            // 
+            lblIpAddressField.AutoSize = true;
+            lblIpAddressField.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblIpAddressField.ForeColor = Color.FromArgb(224, 224, 224);
+            lblIpAddressField.Location = new Point(48, 34);
+            lblIpAddressField.Name = "lblIpAddressField";
+            lblIpAddressField.Size = new Size(217, 21);
+            lblIpAddressField.TabIndex = 6;
+            lblIpAddressField.Text = "endereço ip do computador";
+            // 
+            // lblIpAddress
+            // 
+            lblIpAddress.AutoSize = true;
+            lblIpAddress.BackColor = Color.FromArgb(224, 224, 224);
+            lblIpAddress.ForeColor = Color.FromArgb(64, 64, 64);
+            lblIpAddress.Location = new Point(16, 34);
+            lblIpAddress.Name = "lblIpAddress";
+            lblIpAddress.Size = new Size(26, 21);
+            lblIpAddress.TabIndex = 4;
+            lblIpAddress.Text = "IP:";
+            // 
+            // lblMacAddress
+            // 
+            lblMacAddress.AutoSize = true;
+            lblMacAddress.BackColor = Color.FromArgb(224, 224, 224);
+            lblMacAddress.ForeColor = Color.FromArgb(64, 64, 64);
+            lblMacAddress.Location = new Point(16, 55);
+            lblMacAddress.Name = "lblMacAddress";
+            lblMacAddress.Size = new Size(102, 21);
+            lblMacAddress.TabIndex = 0;
+            lblMacAddress.Text = "Mac Address:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
-            ClientSize = new Size(532, 282);
+            ClientSize = new Size(532, 347);
+            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Controls.Add(lblUserNameField);
             Controls.Add(lblComputerNameField);
@@ -191,6 +323,8 @@
             Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -203,10 +337,20 @@
         private Label lblComputerNameField;
         private Label lblUserNameField;
         private GroupBox groupBox1;
+        private Label lblRam;
+        private Label lblProcessor;
+        private Label lblStorage;
+        private Label lblOperatingSystem;
+        private Label lblRamField;
+        private Label lblStorageField;
+        private Label lblProcessorField;
+        private Label lblOperatingSystemField;
+        private GroupBox groupBox2;
+        private Label lblMacAddressField;
+        private Label lblIpAddressField;
+        private Label lblIpAddress;
+        private Label lblMacAddress;
         private Label label2;
         private Label label1;
-        private Label label3;
-        private Label label4;
-        private Label label5;
     }
 }
--- NetzerClient/Form1.cs
+++ NetzerClient/Form1.cs
@@ -1,3 +1,10 @@
+using System.Management;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+
 namespace NetzerClient
 {
     public partial class Form1 : Form
@@ -10,10 +17,124 @@ namespace NetzerClient
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            lblComputerNameField.Text = Environment.MachineName;
-            lblUserNameField.Text = Environment.UserName;
+            lblComputerNameField.Text = GetMachineName();
+
+            lblUserNameField.Text = GetUserName();
+
+            lblOperatingSystemField.Text = GetOperatingSystem();
+
+            lblProcessorField.Text = GetProcessor();
+
+            lblIpAddressField.Text = GetLocalIPAddress();
+
+            lblMacAddressField.Text = GetMacAddress();
+
+            var requestObject = new
+            {
+                machineName = lblComputerNameField.Text,
+                userName = lblUserNameField.Text,
+                operatingSystem = lblOperatingSystemField.Text,
+                processor = lblProcessorField.Text,
+                ipAddress = lblIpAddressField.Text,
+                macAddress = lblMacAddressField.Text
+            };
+
+            string requestJson = JsonSerializer.Serialize(requestObject);
+
+            MessageBox.Show(requestJson);
+        }
+
+
+
+        private static String GetProcessor()
+        {
+            try
+            {
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select Name, ProcessorId from Win32_Processor"))
+                {
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        return $"{obj["Name"]}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return "Erro ao obter processador";
 
         }
+
+        private static String? GetOperatingSystem()
+        {
+
+            var name = (from x in new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem").Get().Cast<ManagementObject>()
+                        select x.GetPropertyValue("Caption")).FirstOrDefault();
+            return name != null ? name.ToString() : "Unknown";
+
+            //return Environment.OSVersion.ToString();
+        }
+
+        private static String GetMachineName()
+        {
+            return Environment.MachineName;
+        }
+
+        private static String GetUserName()
+        {
+            return Environment.UserName;
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private string GetMacAddress()
+        {
+            string macAddresses = string.Empty;
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+
+            return macAddresses;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private void lblTitle_Click(object sender, EventArgs e)
         {
@@ -21,6 +142,11 @@ namespace NetzerClient
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
         {
 
         }
